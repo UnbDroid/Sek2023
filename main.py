578f9d80f8d6935641a51e57f13fc96bcab648f7
Pedro Henrique Alves Martos <phm.pedroalves@gmail.com>
2023-07-25 11:52:33 -0300

    Sensores Laterias

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.parameters import Port
+from pybricks.nxtdevices import ColorSensor
 
+
+
+# Defininindo as portas
 ev3 = EV3Brick()
+color_upper = ColorSensor(Port.S4)
+color_lower = ColorSensor(Port.S3)
 
+# Programando
+while True:
+    if color_lower.reflection() > 10 and color_upper.reflection() > 7:
+        print("Tubo de 15 :) ")
+        
+    if  color_lower.reflection() > 10 and color_upper.reflection () < 7:
+        print("Tubo de 10 :) ") 
+        
+        
