c01b4591ce3a92b481c62cac1c6e5e752917f3e8
CaldoO-O <gabcaldo@droidunb.com>
2023-08-24 09:21:04 -0300

    Main organizada

--- main.py
+++ main.py
@@ -9,22 +9,9 @@ from modules.claw import *
 from pybricks.hubs import EV3Brick
 from pybricks.tools import wait
 
-# # In this program, the server waits for the client to send the first message
-# # and then sends a reply.
-# mbox.wait()
-# print(mbox.read())
-# mbox.send('hello to you!')
-
 ev3 = EV3Brick()
-# Open()
-# Close()
 
-#teste
-# cronometer.reset()
-# while cronometer.time() < 10000:
-#     andar_reto(50)
-
-#Codigo
+# Codigo ---------------------------------------------------------------------------------------
 
 find_blue_line()
 print("RGB Esquerdo: ", red_left(), green_left(), blue_left(), "RGB Direito: ", red_right(), green_right(), blue_right())
@@ -39,37 +26,41 @@ while True:
     set_path()
     ev3.speaker.beep(444, 1000)
 
+#-----------------------------------------------------------------------------------------------
 
+
+
+# Abrir ou fechar a garra ----------------------------------------------------------------------
+
+# Open()
+# Close()
+
+#-----------------------------------------------------------------------------------------------
+
+
+
+# Ajustar ranges das cores ---------------------------------------------------------------------
+
+# while True:
+#     print("Esquerda: ", color_sensor_floor_left.rgb() , "Direita: ", color_sensor_floor_right.rgb())
+
+#-----------------------------------------------------------------------------------------------
+
+
+
+# Ajustar ranges dos tubos ---------------------------------------------------------------------
 
 # while True:
 #     print(tube_verificator.rgb())
+
+#-----------------------------------------------------------------------------------------------
+
+
+
+# Andar reto -----------------------------------------------------------------------------------
+
+# cronometer.reset()
+# while cronometer.time() < 10000:
 #     andar_reto(50)
-# print("Achou vermelho", cronometer.time())
 
-
-# while True:
-#     left_motor.run(50)
-#     print(left_motor.speed())
-
-# while True:
-#     motors.turn(360)
-#     motors.stop()
-#     wait(1000)
-
-# cout=0
-# while cronometer.time() < 5000:
-#     andar_reto(40)
-#     print("Esquerda: ", color_sensor_floor_left.rgb() , "Direita: ", color_sensor_floor_right.rgb())
-#     cout+=1
-    
-# print(cout)
-
-# while True:
-#     print("Esquerda: ", color_sensor_floor_left.rgb() , "Direita: ", color_sensor_floor_right.rgb())
-
-# tube_presence = tube_verificator.reflection()
-# while tube_presence == 0: 
-#     tube_presence = tube_verificator.reflection()
-#     scan()
-
-# turn_right(90)
+#-----------------------------------------------------------------------------------------------
