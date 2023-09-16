6d93629f33ab74f696965e34d26a0941d5131f46
Pedro Henrique Alves Martos <phm.pedroalves@gmail.com>
2023-09-16 08:00:09 -0300

    Organizei algumas coisas do código. Precisamos ajustar a cor marrom dos tubos e incrementar no código

--- modules/beeps.py
+++ modules/beeps.py
@@ -1,11 +1,15 @@
 from pybricks.hubs import EV3Brick
+from pybricks.tools import wait
+
 ev3 = EV3Brick()
 
 def found_door():
     ev3.speaker.beep(100, 1000)
     
 def found_wall():
-    ev3.speaker.beep(1000, 1000)
+    ev3.speaker.beep(1000, 500)
+    wait(250)
+    ev3.speaker.beep(1000, 500)
     
 def not_found_wall():
     ev3.speaker.beep(100, 1000)
