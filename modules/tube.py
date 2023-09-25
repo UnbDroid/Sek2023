610b36e183159415c8dccb94bf9f30ed25cdb51a
Pedro Henrique Alves Martos <phm.pedroalves@gmail.com>
2023-09-25 15:27:14 -0300

    Testando a captura lateral!

--- modules/tube.py
+++ modules/tube.py
@@ -2,6 +2,10 @@ from pybricks.nxtdevices import ColorSensor
 from pybricks.parameters import Port
 
 tube_sensor_verification = ColorSensor(Port.S3)
+scan_sensor = ColorSensor(Port.S4) # Teste de sensor de cor 
+
+
+
 
 def tube():
     verification = tube_sensor_verification.reflection()
@@ -11,3 +15,12 @@ def tube():
     else:
         print("Tube is 10cm tall")
         return False
+
+
+def tube_scan():
+    verification = scan_sensor.reflection()
+    if verification != 1:
+        print("Tem um tubo ai rapaz!")
+        
+    else:
+        print("To vendo nada não chefe!")
