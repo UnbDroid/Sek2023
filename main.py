0c55ccf2b85fcb03cd320b5857dff501d1c88fa6
Pedro Henrique Alves Martos <phm.pedroalves@gmail.com>
2023-10-09 02:13:08 -0300

    Alguem me tira daqui

--- main.py
+++ main.py
@@ -13,49 +13,49 @@ from pybricks.messaging import BluetoothMailboxClient, TextMailbox
 print("2")
 from modules.tube import *
 
-ev3 = EV3Brick()
+# ev3 = EV3Brick()
 
-SERVER = 'ev3dev'
-print("3")
-client = BluetoothMailboxClient()
-mbox = TextMailbox('greeting', client)
+# SERVER = 'ev3dev'
+# print("3")
+# client = BluetoothMailboxClient()
+# mbox = TextMailbox('greeting', client)
 
-print('establishing connection...')
-client.connect(SERVER)
-print('connected!')
+# print('establishing connection...')
+# client.connect(SERVER)
+# print('connected!')
 
-while True:
-    mbox.wait()
-    msg = mbox.read()
-    
-    if msg == 'alinhar':
-        mbox.send(alinhar_azul())
-        
-    elif msg == "chave":
-        if tube() == True:
-            mbox.send('15')
-        else:
-            mbox.send('10')
-    
-    elif msg == "de_ladinho":
-        if scan_de_ladinho() == True:
-            mbox.send('Vi tubo')
-        else:
-            mbox.send('Sem tubo')
-        
-    elif msg == "tem tubo?":
-        if tube_is_detected() == True:
-            ev3.screen.print("tem tubo")
-            mbox.send("tem tubo")
-        else:
-            ev3.screen.print("nao tem tubo")
-            mbox.send("nao tem tubo")
-         
-    elif msg == "cor do tubo":
-        mbox.send(color_tube())
-         
-    elif msg == "scan":
-        tube_scan()
-    
 # while True:
-#     print(tube_presence_verificator.rgb())
+#     mbox.wait()
+#     msg = mbox.read()
+    
+#     if msg == 'alinhar':
+#         mbox.send(alinhar_azul())
+        
+#     elif msg == "chave":
+#         if tube() == True:
+#             mbox.send('15')
+#         else:
+#             mbox.send('10')
+    
+#     elif msg == "de_ladinho":
+#         if scan_de_ladinho() == True:
+#             mbox.send('Vi tubo')
+#         else:
+#             mbox.send('Sem tubo')
+        
+#     elif msg == "tem tubo?":
+#         if tube_is_detected() == True:
+#             ev3.screen.print("tem tubo")
+#             mbox.send("tem tubo")
+#         else:
+#             ev3.screen.print("nao tem tubo")
+#             mbox.send("nao tem tubo")
+         
+#     elif msg == "cor do tubo":
+#         mbox.send(color_tube())
+         
+#     elif msg == "scan":
+#         tube_scan()
+    
+while True:
+    print(scan_de_ladinho())
